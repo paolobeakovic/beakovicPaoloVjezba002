b152e596e791146709e36f2666cc5753384481d8
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-03-10 11:44:42 +0100

    Add files via upload

---
+++ beakovicPaoloVjezbaOO2/Vozilo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beakovicPaoloVjezbaOO2
+{
+    class Vozilo
+    {
+        string vrata;
+        int maxBrzina;
+        int brojKotaca;
+
+        public Vozilo(string vrata, int maxBrzina, int brojKotaca)
+        {
+            this.vrata = vrata;
+            this.maxBrzina = maxBrzina;
+            this.brojKotaca = brojKotaca;
+        }
+
+        public Vozilo(string vrata, int brojKotaca)
+        {
+            this.vrata = vrata;
+            this.brojKotaca = brojKotaca;
+        }
+
+        public void setmaxBrzina(int maxBrzina)
+        {
+            this.maxBrzina = maxBrzina;
+        }
+
+
+    }
+}
